ef25f14e945eb70feac69aeecc121a92f53d59b0
DegrangeM <53106394+DegrangeM@users.noreply.github.com>
2022-05-09 23:10:58 +0200

    Update background.js

--- background.js
+++ background.js
@@ -2,10 +2,11 @@ async function click(tab) {
 
     const composeDetails = await messenger.compose.getComposeDetails(tab.id);
 
-    for (let k = 0; k < composeDetails.to.length; k++) { //let dest of composeDetails.to) {
+    for (let k = 0; k < composeDetails.to.length; k++) {
         let dest = composeDetails.to[k];
+        // contact can either be a string (entered by user) or an object (from address book)
         if (typeof dest === 'string') {
-            if (!dest.includes('@')) {
+            if (!dest.includes('@')) { // Only search recipient which doesn't already have an email address
                 dest = dest.replace('<>', '');
                 const contacts = await messenger.contacts.quickSearch(null, dest);
                 if (contacts.length === 1) {
