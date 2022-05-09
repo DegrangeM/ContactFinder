f0b673a43f1275da1c77a4a292abf6eb4a2fa851
DegrangeM <53106394+DegrangeM@users.noreply.github.com>
2022-05-09 22:10:49 +0200

    Bouton immédiat au lieu de popup

---
+++ background.js
@@ -0,0 +1,33 @@
+async function click(tab) {
+
+    const composeDetails = await messenger.compose.getComposeDetails(tab.id);
+
+    for (let k = 0; k < composeDetails.to.length; k++) { //let dest of composeDetails.to) {
+        let dest = composeDetails.to[k];
+        if (typeof dest === 'string') {
+            if (!dest.includes('@')) {
+                dest = dest.replace('<>', '');
+                const contacts = await messenger.contacts.quickSearch(null, dest);
+                if (contacts.length === 1) {
+                    composeDetails.to[k] = {
+                        id: contacts[0].id,
+                        type: 'contact'
+                    }
+                } else {
+                    if (contacts.length === 0) {
+                        dest = dest + ' <not found>';
+                    } else {
+                        dest = dest + ' <several found>';
+                    }
+                    composeDetails.to[k] = dest;
+                }
+            }
+        }
+    }
+
+    await messenger.compose.setComposeDetails(tab.id, {
+        to: composeDetails.to
+    });
+    
+}
+messenger.composeAction.onClicked.addListener(click);
